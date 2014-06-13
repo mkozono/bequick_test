56dfb4ef52d79551631263d381f143313e8b7f5d
Michael Kozono <mkozono@gmail.com>
2014-06-13 12:30:46 -0700

    Have two failing integration tests, now fill in

--- sequencer.rb
+++ sequencer.rb
@@ -1,3 +1,17 @@
 class Sequencer
 
+  def initialize(dictionary_filename, sequence_filename, words_filename)
+    @dictionary_filename = dictionary_filename
+    @sequence_filename = sequence_filename
+    @words_filename = words_filename
+  end
+
+  def process_four_letter_sequences
+    File.open(@sequence_filename, 'w') do |file|
+      file.write('hello')
+    end
+    File.open(@words_filename, 'w') do |file|
+      file.write('hello')
+    end
+  end
 end
--- spec/sequencer_spec.rb
+++ spec/sequencer_spec.rb
@@ -1,9 +1,36 @@
 require "./sequencer.rb"
+require 'fakefs/safe'
 
 describe Sequencer do
-  describe "some method" do
-    it "does something" do
-      # pass
+  describe "process_four_letter_sequences" do
+    before do
+      FakeFS.activate!
+      File.open("example_dictionary.txt", 'w') do |file|
+        file.write("arrows\ncarrots\ngive\nme\n")
+      end
+      @sequencer = Sequencer.new("example_dictionary.txt", "sequences", "words")
+      @sequencer.process_four_letter_sequences
+    end
+    after do
+      FakeFS.deactivate!
+    end
+    it "creates the sequences file" do
+      expect(File.exists?('sequences')).to eq true
+    end
+    it "creates the words file" do
+      expect(File.exists?('words')).to eq true
+    end
+    context "sequences file" do
+      it "contains the unique four letter sequences" do
+        content = File.read("sequences")
+        expect(content).to eq "carr\ngive\nrots\nrows\nrrot\nrrow\n"
+      end
+    end
+    context "words file" do
+      it "contains the words corresponding to the sequences" do
+        content = File.read("words")
+        expect(content).to eq "carrots\ngive\ncarrots\narrows\ncarrots\narrows\n"
+      end
     end
   end
 end
